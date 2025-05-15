c816c5b5f4d64b54d8f10a16cb161a9437806791
Gatien DIDRY <gatien.didry@orange.fr>
2025-05-15 09:33:31 +0200

    initial commit

---
+++ stories_test_didry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stories_test_didry
+//
+//  Created by Gatien DIDRY on 15/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stories_test_didry/stories_test_didryApp.swift
@@ -0,0 +1,17 @@
+//
+//  stories_test_didryApp.swift
+//  stories_test_didry
+//
+//  Created by Gatien DIDRY on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct stories_test_didryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
